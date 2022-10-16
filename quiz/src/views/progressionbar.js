d05ce53a024cb6b1a0a73c9f7d07a1e4511af34f
Abel <abelbeyene373@gmail.com>
2022-10-16 23:54:43 +0200

    change

--- quiz/src/views/progressionbar.js
+++ quiz/src/views/progressionbar.js
@@ -1,19 +1,19 @@
 
-import { quizData } from '../data.js';
+// import { quizData } from '../data.js';
 
-export const progressionBar = (myParent,myIndex) => {
-  const userParent = document.getElementById(myParent);
+// export const progressionBar = (myParent,myIndex) => {
+//   const userParent = document.getElementById(myParent);
 
-  const progressionDiv = document.createElement('div');
-  progressionDiv.classList.add('progress-container');
-  const progress = document.createElement('div');
-  progress.classList.add('progress');
+//   const progressionDiv = document.createElement('div');
+//   progressionDiv.classList.add('progress-container');
+//   const progress = document.createElement('div');
+//   progress.classList.add('progress');
 
-  userParent.appendChild(progressionDiv);
-  progressionDiv.appendChild(progress);
-  let num = 43;
+//   userParent.appendChild(progressionDiv);
+//   progressionDiv.appendChild(progress);
+//   let num = 43;
 
-  num *= myIndex;
+//   num *= myIndex;
 
-  progress.style.width =` ${num}px`;
-};
+//   progress.style.width =` ${num}px`;
+// };
