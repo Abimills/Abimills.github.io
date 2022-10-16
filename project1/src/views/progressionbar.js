b405a6c7f1dfc8f60392767dcf96f64be3688c61
Abel <abelbeyene373@gmail.com>
2022-10-16 22:32:03 +0200

    removed progression

--- project1/src/views/progressionbar.js
+++ project1/src/views/progressionbar.js
@@ -1,19 +1,19 @@
 
-import { quizData } from '../data.js';
+// import { quizData } from '../data.js';
 
-export const progressionBar = () => {
-  const userInterface = document.getElementById('user-interface');
+// export const progressionBar = () => {
+//   const userInterface = document.getElementById('user-interface');
 
-  const progressionDiv = document.createElement('div');
-  progressionDiv.classList.add('progress-container');
-  const progress = document.createElement('div');
-  progress.classList.add('progress');
+//   const progressionDiv = document.createElement('div');
+//   progressionDiv.classList.add('progress-container');
+//   const progress = document.createElement('div');
+//   progress.classList.add('progress');
 
-  userInterface.appendChild(progressionDiv);
-  progressionDiv.appendChild(progress);
-  let num = 43;
+//   userInterface.appendChild(progressionDiv);
+//   progressionDiv.appendChild(progress);
+//   let num = 43;
 
-  num *= quizData.currentQuestionIndex;
+//   num *= quizData.currentQuestionIndex;
 
-  progress.style.width = num + 'px';
-};
+//   progress.style.width = num + 'px';
+// };
